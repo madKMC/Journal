734f234048e30b16924143e486cdb53cd583ac4c
Kaylen <63236935+madKMC@users.noreply.github.com>
2025-06-29 23:52:16 +0200

    Fix Tailwind config TypeScript error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+const config = {
   darkMode: ['class'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -173,4 +172,5 @@ const config: Config = {
   },
   plugins: [require('tailwindcss-animate')],
 };
-export default config;
+
+module.exports = config;
